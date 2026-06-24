9480d7ee79972d66bbf99443be95b8a0c7f5cb17
skibidi <mariia.melesh.25@kse.org.ua>
2026-06-24 16:17:02 +0300

    added different type serialiation

--- line.cpp
+++ line.cpp
@@ -5,16 +5,34 @@
 class Line {
 public:
     virtual ~Line() = default;
+	virtual std::string serialize() const = 0;
 };
 
 class Text : public Line {
-	
-}
+private:
+    std::string text;
+public:
+	std::string serialize() const override { 
+		return "text:" + text; 
+	}
+};
  
 class Checklist : public Line {
-	
-}
+private:
+    std::string task;
+	bool checked;
+public:
+	std::string serialize() const override { 
+		return "checklist:" + checked ? "1 ," : "0 ," + task; 
+	}
+};
 
 class Contact : public Line {
-	
-}
+private:
+    std::string name;
+    std::string email;
+public:
+	std::string serialize() const override { 
+		return "contact:" + name + "," + email;
+	}
+};
