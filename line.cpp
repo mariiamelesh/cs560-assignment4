a6b3bdf4c508eee2f085fcfb34f589af4ddd17fb
skibidi <mariia.melesh.25@kse.org.ua>
2026-06-24 17:02:37 +0300

    implemented print() function to show lines

--- line.cpp
+++ line.cpp
@@ -5,6 +5,7 @@
 class Line {
 public:
     virtual ~Line() = default;
+	virtual void print(bool hasCursor, int cursorIndex) const = 0;
 	virtual std::string serialize() const = 0;
 };
 
@@ -12,6 +13,20 @@ class Text : public Line {
 private:
     std::string text;
 public:
+	TextLine(const std::string& t) {
+		text =t;
+	}
+
+	void print(bool hasCursor, int cursorIndex) const override {
+		std::cout << "Text: ";
+        for (size_t i = 0; i < text.length(); ++i) {
+            if (hasCursor && (int)i == cursorIndex) {
+				std::cout << "[" << text[i] << "]";
+			} else {
+				std::cout << text[i];
+			}
+        }
+	}
 	std::string serialize() const override { 
 		return "text:" + text; 
 	}
@@ -22,6 +37,15 @@ private:
     std::string task;
 	bool checked;
 public:
+	Checklist(const std::string& i, bool c) {
+		item = i;
+		checked = c;
+	}
+
+	void print(bool hasCursor, int cursorIndex) const override {
+        std::cout << "[ " << (checked ? "x" : " ") << " ] " << item << std::endl;
+    }
+
 	std::string serialize() const override { 
 		return "checklist:" + checked ? "1 ," : "0 ," + task; 
 	}
@@ -32,6 +56,15 @@ private:
     std::string name;
     std::string email;
 public:
+	Contact(const std::string& n, const std::string& em) {
+		name = n;
+		email = em;
+	}
+
+    void print(bool hasCursor, int cursorIndex) const override {
+        std::cout << "Contact - " << name << ", E-mail: " << email << std::endl;
+    }
+
 	std::string serialize() const override { 
 		return "contact:" + name + "," + email;
 	}
