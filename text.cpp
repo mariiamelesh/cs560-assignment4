e612e7b02ebf53d0358602e821e1559d5f52db29
skibidi <mariia.melesh.25@kse.org.ua>
2026-06-24 17:24:52 +0300

    added deserialize() func

--- text.cpp
+++ text.cpp
@@ -21,4 +21,30 @@ public:
         }
         return result;
     }
+	void deserialize(const std::string& text) {
+        clear();
+        std::stringstream ss(text);
+        std::string row;
+        while (std::getline(ss, row)) {
+            if (row.empty()) {
+				continue;
+			}
+            
+            if (row.rfind("text:", 0) == 0) {
+                addLine(std::make_unique<TextLine>(row.substr(5)));
+            } 
+            else if (row.rfind("checklist:", 0) == 0) {
+                bool checked = (row[4] == '1');
+                addLine(std::make_unique<ChecklistLine>(row.substr(12), checked));
+            } 
+            else if (row.rfind("contact:", 0) == 0) {
+                size_t comma = row.find(',', 8);
+                if (comma != std::string::npos) {
+                    std::string name = row.substr(8, comma - 8);
+                    std::string email = row.substr(comma + 1);
+                    addLine(std::make_unique<ContactLine>(name, email));
+                }
+            }
+        }
+    }
 }
