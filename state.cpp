6f85766ad9fd45ebb487fae513faae346e8ed401
skibidi <mariia.melesh.25@kse.org.ua>
2026-06-24 15:03:06 +0300

    created state managing classes

---
+++ state.cpp
@@ -0,0 +1,16 @@
+#pragma once
+#include <vector>
+#include <string>
+
+class Cursor {
+public:
+    int currentLine = 0;
+    int currentIndex = 0;
+};
+
+class Copy {
+};
+
+class History {
+   
+};
