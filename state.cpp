0b8c6554259d7770d352440d8f2cb49706fde159
skibidi <mariia.melesh.25@kse.org.ua>
2026-06-24 15:07:05 +0300

    implemented cursor functions

--- state.cpp
+++ state.cpp
@@ -6,6 +6,26 @@ class Cursor {
 public:
     int currentLine = 0;
     int currentIndex = 0;
+	
+	void left() { 
+		if (currentIndex > 0) {
+			currentIndex--; 
+		}
+	}
+    void right(int lineLen) {
+		if (lineLen > 0 && currentIndex < lineLen - 1) {
+			currentIndex++; 
+		}
+	}
+    void up() { 
+		if (currentLine > 0) {
+			currentLine--; 
+		}
+	}
+    void down(int totalLines) { 
+		if (totalLines > 0 && currentLine < totalLines - 1) { 
+			currentLine++; 
+	}
 };
 
 class Copy {
