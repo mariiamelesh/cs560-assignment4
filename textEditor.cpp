bf776d46b1c26926de63ca16741b1cc450118175
skibidi <mariia.melesh.25@kse.org.ua>
2026-06-24 15:45:19 +0300

    included implemented Text Cursor and History classes

--- textEditor.cpp
+++ textEditor.cpp
@@ -1,5 +1,10 @@
 #include <fstream>
-
+#include "line.h"
+#include "text.h"
+#include "state.h"
 class TextEditor {
-
+    Text doc;
+    Cursor cursor;
+    History history;
+    std::string clipboard;
 };
